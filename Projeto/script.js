f4c395fa7a00cd689d4bbb2b458cb41064c3bb7c
wjuniorhp <wjuniorhp@gmail.com>
2026-03-17 21:40:49 -0300

    css + tratamento

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,19 +1,31 @@
 const nomePrato = document.querySelector('#nome');
 const precoPrato = document.querySelector('#preco');
 const imagemPrato = document.querySelector('#imagem');
+const descricaoPrato = document.querySelector('#descricao');
 const botaoCadastrar = document.querySelector('#botao-cadastrar');
 const cardapio = document.querySelector('#cardapio');
 
 class Prato {
-    constructor(nome, preco, imagem) {
+    constructor(nome, preco, imagem, descricao) {
         this.nome = nome;
         this.preco = preco;
         this.imagem = imagem;
+        this.descricao = descricao;
+        this.validarPrato()
+        
 
         // Salvar o objeto dentro de um array no local storage
         const pratos = JSON.parse(localStorage.getItem('cardapio')) || [];
         pratos.push(this);
         localStorage.setItem('cardapio', JSON.stringify(pratos));
+    }
+
+    validarPrato() {
+        if (!this.nome || !this.preco || !this.imagem || !this.descricao) {
+            alert('Por favor, preencha todos os campos para cadastrar o prato.');
+            throw new Error('Todos os campos são obrigatórios');
+        }
+        return true;
     }
 
     // criarLi() {
@@ -29,12 +41,15 @@ class Prato {
 
 function renderizarCardapio() {
     ul.innerHTML = '';
+    ul.classList.add('cardapio-ul');
     const pratos = JSON.parse(localStorage.getItem('cardapio')) || [];
     pratos.forEach(prato => {
         const li = document.createElement('li');
+        li.classList.add('prato');
         li.innerHTML = `
             <h2>${prato.nome}</h2>
-            <p>${prato.preco}</p>
+            <h3>R\$ ${Number(prato.preco).toFixed(2)}</h3>
+            <p>${prato.descricao}</p>
             <img src="${prato.imagem}" alt="${prato.nome}">
         `;
         ul.appendChild(li);
@@ -42,7 +57,12 @@ function renderizarCardapio() {
 }
 
 botaoCadastrar.addEventListener('click', () => {
-    const prato = new Prato(nomePrato.value, precoPrato.value, imagemPrato.value);
+    try {
+        const prato = new Prato(nomePrato.value, precoPrato.value, imagemPrato.value, descricaoPrato.value);
+    } catch (error) {
+        console.error(error);
+        return;
+    }
     renderizarCardapio();
 });
 
